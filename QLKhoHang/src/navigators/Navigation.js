eb235305432ba1cd9e3248f2a6cc2ac40d56e5ce
lethai123 <21211tt4821@mail.tdc.edu.vn>
2023-11-01 09:14:18 +0700

    Fix

--- QLKhoHang/src/navigators/Navigation.js
+++ QLKhoHang/src/navigators/Navigation.js
@@ -22,7 +22,7 @@ const Navigation = () => {
     <NavigationContainer>
       {/* initialRouteName='Welcome' */}
       <Stack.Navigator initialRouteName='Home'>
-        {userInfo.accessToken ? (
+        {userInfo.access_Token ? (
           <Stack.Screen name="HomeNavigation" component={HomeNavigation} options={{ headerShown: false }} />
         ) : (
           <>
