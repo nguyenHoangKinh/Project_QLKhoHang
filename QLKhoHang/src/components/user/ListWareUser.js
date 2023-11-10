91dc797463d3f89715904c51260ad9a72a3d4b72
NguyenVoBaoPhi <phipk456@gamil.com>
2023-11-11 02:01:30 +0700

    lay tahnh cong ten chukho qua id owners

--- QLKhoHang/src/components/user/ListWareUser.js
+++ QLKhoHang/src/components/user/ListWareUser.js
@@ -8,28 +8,16 @@ import {
   View,
 } from "react-native";
 import React, { useState, useContext, useEffect } from "react";
-// import AppStyle from "../../theme";
-
 import axios from "axios";
 import { AuthContext } from "../../context/AuthContext";
 import AppStyle from "../../theme";
 import StyleListWareHouse from "../../theme/StyleListWareHouse";
 
-// // console.log(Date.now().toString());
-// export default function ListWareUser() {
-
-// }
-
-// const ListWareUser = ({navigation}) => {
-//   // Init local states
-
-// };
+let owners = [];
 
 export default function ListWareUser() {
   const [list, setList] = useState({});
   const { userInfo } = useContext(AuthContext);
-
-  //   console.log(userInfo.accessToken)
 
   useEffect(() => {
     axios
@@ -37,77 +25,78 @@ export default function ListWareUser() {
         `https://warehouse-management-api.vercel.app/v1/warehouse/listWarehouseUser`,
         {
           headers: {
-            Authorization: `Token ${userInfo.accessToken}`,
+            Authorization: `Bearer ${userInfo.accessToken}`,
           },
         }
       )
       .then((res) => {
-        //console.log(warehouses)
         let warehouseUser = res.data.warehouse;
         setList(warehouseUser);
-        console.log(res.data.warehouse);
       })
       .catch((e) => {
         console.log(`get warehouseUser error ${e.res}`);
-        //console.log(e);
       });
   }, []);
+  
+  for (let i = 0; i < list.length; i++) {
+    axios
+      .get(
+        `https://warehouse-management-api.vercel.app/v1/auth/account-by-id`,
+        {
+          headers: {
+            Authorization: `Bearer ${userInfo.accessToken}`,
+          },
+          params: {
+            id: list[i].owner,
+          },
+        }
+      )
+      .then((res) => {
+        let owner = res.data.others.username;
+        // console.log(owner)
+        owners.push(<Text key={i}>{owner}</Text>);
+      })
+      .catch((e) => {
+        console.log(`get owner error ${e.res}`);
+      });
+  }
 
+  console.log(owners)
   // Render todo
   const renderTodos = ({ item, index }) => {
+    
     return (
-      //   <View style={StyleListWareHouse.container}>
-      //     <Text style={StyleListWareHouse.nameProduct}>Tên:
-      //       {item.wareHouseName}
-      //     </Text>
-      //     <Text style={StyleListWareHouse.nameProduct}>
-      //       Địa chỉ:{item.address}
-      //     </Text>
-      //     <Text style={StyleListWareHouse.nameProduct}>
-      //       Danh mục{item.category}
-      //     </Text>
-      //     <Text style={StyleListWareHouse.nameProduct}>
-      //       Giá:{item.monney}
-      //     </Text>
-      //     <Text style={StyleListWareHouse.nameProduct}>
-      //         Chủ kho:
-      //       {item.owner}
-      //     </Text>
-      //   </View>
       <Pressable>
         <View style={AppStyle.StyleOderList.item}>
           <View style={AppStyle.StyleOderList.rightItem}>
             {/* <View className="flex flex-row justify-between"> */}
-                <View>
-                <Text style={AppStyle.StyleOderList.text}>
-              Tên:
-              {item.wareHouseName} -
-              Địa chỉ:{item.address}
-            </Text>
-            <Text style={AppStyle.StyleOderList.text}>
-              Danh mục{item.category}, Gia{item.monney}
-            </Text>
-            <Text style={AppStyle.StyleOderList.text}>
-              Chủ kho:
-              {item.owner}
-            </Text>
-                </View>
-                {/* <View>
-                <Text style={AppStyle.StyleOderList.text}>
-              Tên:
-              {item.wareHouseName}
-            </Text>
-                </View> */}
-            {/* </View> */}
+            <View>
+              <Text style={AppStyle.StyleOderList.text}>
+                Tên:
+                {item.wareHouseName}
+              </Text>
+              <Text style={AppStyle.StyleOderList.text}>
+                Địa chỉ:{item.address}
+              </Text>
+              <Text style={AppStyle.StyleOderList.text}>
+                Danh mục: {item.category}
+              </Text>
+              <Text style={AppStyle.StyleOderList.text}>
+                Gia :{item.monney}
+              </Text>
+              <Text style={AppStyle.StyleOderList.text}>
+                Chủ kho:
+                {owners[index]}
+              </Text>
+            </View>
           </View>
         </View>
       </Pressable>
     );
   };
-  return (
-    <View style={{ marginHorizontal: 16, marginTop: 40 }}>
-      {/* Render todo list */}
 
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <FlatList data={list} renderItem={renderTodos} />
     </View>
   );
